cb2d4b3c3ffc63fd119a29754ffbf2785750eefd
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-11 16:58:11 -0600

    Add AuthMiddleware function to middleware package

--- api-v1/middleware/middleware.go
+++ api-v1/middleware/middleware.go
@@ -1,1 +1,13 @@
 package middleware
+
+import (
+	"fmt"
+
+	"github.com/danielgtaylor/huma/v2"
+)
+
+func AuthMiddleware(ctx huma.Context, next func(huma.Context)) {
+
+	fmt.Println("AuthMiddleware")
+
+}
