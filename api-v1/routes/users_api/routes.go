2a96abad4936a1d80614ba2ae8518923515be738
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-11 16:59:43 -0600

    Add AuthMiddleware to users profile route

--- api-v1/routes/users_api/routes.go
+++ api-v1/routes/users_api/routes.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/danielgtaylor/huma/v2"
+
+	"api-go/middleware"
 )
 
 func Register(api huma.API) {
@@ -13,6 +15,7 @@ func Register(api huma.API) {
 		Method:      http.MethodGet,
 		Path:        "/users/profile",
 		Tags:        []string{"users"},
+		Middlewares: huma.Middlewares{middleware.AuthMiddleware},
 	}, ProfileGetAPI)
 
 	huma.Register(api, huma.Operation{
