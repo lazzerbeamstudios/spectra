aeb601a862715f0c0e1347e2ffd88c8de938ab43
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-24 00:58:33 -0600

    Refactor database handling by removing Ent integration and retaining Bun setup

--- api-v1/utils/db/bun.go
+++ api-v1/utils/db/bun.go
@@ -3,34 +3,13 @@ package db
 import (
 	"database/sql"
 
-	"entgo.io/ent/dialect"
-	entSQL "entgo.io/ent/dialect/sql"
-
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
-
-	"api-go/ent"
 )
 
-var EntDB *ent.Client
 var BunDB *bun.DB
-
-func SetDB(urlDB string) {
-	connDB, err := sql.Open("pgx", urlDB)
-	if err != nil {
-		panic("Failed to open database connection")
-	}
-
-	err = connDB.Ping()
-	if err != nil {
-		panic("Failed to ping database")
-	}
-
-	sqlDB := entSQL.OpenDB(dialect.Postgres, connDB)
-	EntDB = ent.NewClient(ent.Driver(sqlDB))
-}
 
 func SetBunDB(urlDB string) {
 	sqlDB := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(urlDB)))
