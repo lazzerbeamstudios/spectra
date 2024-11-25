94ec7a9f31a94f3ec662a3c1439ef0d8ab849a4a
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-25 10:31:04 -0600

    Update Docker image tag to version ute1732552258 across configuration files

--- docker_buildx.sh
+++ docker_buildx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --file Dockerfile . --tag lazzerbeam/spectra:ute1732432112 --platform linux/amd64,linux/arm64 --push
+docker buildx build --file Dockerfile . --tag lazzerbeam/spectra:ute1732552258 --platform linux/amd64,linux/arm64 --push
